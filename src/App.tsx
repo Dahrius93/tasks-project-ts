8d70f60a5b0ca13a011ee9c040034370921e6d41
Dahrius93 <dariolauretta93@gmail.com>
2026-05-10 15:26:30 +0200

    compl

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import type { Task } from "./types";
 import Form from "./Form";
 import List from "./List";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   // stato dei tasks creati
-  // alla modifica aggiorno la pagina
-  const [tasks, setTasks] = useState<Task[]>([]);
+  // stato iniziale dipende se ho localStorage altrimenti vuoto
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const stored = localStorage.getItem("tasks");
+    return stored ? JSON.parse(stored) : [];
+  });
 
-  // funzione richiamata dal componente Form
-  // Form richiama la funzione passando la descrizione del nuovo task da aggiungere
+  // ad ogni modifica dei tasks salvo su localStorage
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   const addTask = (description: string): void => {
-    // creo il nuovo task con la decrizione passata dal Form
-    // e aggiorno i tasks
     const newTask: Task = { id: Date.now(), description, isCompleted: false };
     setTasks([...tasks, newTask]);
   };
@@ -23,7 +27,7 @@ function App() {
   // ritorna il task così com'è
   const toggleTask = (id: number) => {
     const newTasks = tasks.map((task) => {
-      if (task.id == id) {
+      if (task.id === id) {
         const newTask: Task = {
           id: task.id,
           description: task.description,
